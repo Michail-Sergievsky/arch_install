c19f2ad7373c1787d6b68447c0436fada28ac306
Michail-Sergievsky <mikhail.sergiev@gmail.com>
2021-06-29 16:08:41 +0300

    add swapfile.sh

---
+++ swapfile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /
+touch swapfile
+#change size
+dd if=/dev/zero of=swapfile bs=1G count=8 status=progress
+chmod 600 swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "#swapfile" >> /etc/fstab
+echo "\/swapfile none swap defaults 0 0" >> /etc/fstab
+
