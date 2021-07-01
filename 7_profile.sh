6aa93a323b9ae6e4187ef2a65e063a6aa6e08037
Michail-Sergievsky <mikhail.sergiev@gmail.com>
2021-07-01 08:53:55 +0300

    Edit profile

--- 7_profile.sh
+++ 7_profile.sh
@@ -6,5 +6,6 @@ mkdir -p $HOME/Pictures/{Wall_use,screenshots,IMAGES_COPIED,IMAGES_SELECTED}
 mkdir -p $HOME/Downloads/{t_done,t_work,comics}
 mkdir -p $HOME/NetFolders/{torrents,syl-storage} 
 mkdir -p $HOME/Soft
+mkdir -p $HOME/Temp
 sudo mkdir -p /run/media/freeman
 ln -s /run/media/freeman ~/Mounted
