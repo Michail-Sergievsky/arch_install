8445e2f5e1d936d69951ac2a389f0da1b51ce75d
Michail-Sergievsky <mikhail.sergiev@gmail.com>
2022-06-28 22:50:53 +0300

    Add root profile for vim,vifm

--- 7_profile.sh
+++ 7_profile.sh
@@ -10,5 +10,10 @@ mkdir -p $HOME/Soft
 mkdir -p $HOME/Stuff
 mkdir -p $HOME/Temp
 mkdir -p $HOME/.vim/undodir
+sudo cp /home/freeman/.vim /root/
+sudo mkdir /root/.config
+sudo cp -r /home/freeman/.config/vifm /root/.config/
+sudo sed -i 's/colorscheme solarized-light-256 Default-256 Default/" colorscheme solarized-light-256 Default-256 Default/' /root/.config/vifm/vifmrc 
+sudo sed -i 's/" colorscheme solarized-light-256-r.vifm/colorscheme solarized-light-256-r.vifm/' /root/.config/vifm/vifmrc 
 sudo mkdir -p /run/media/freeman
 ln -s /run/media/freeman ~/Mounted
