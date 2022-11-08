4e7bc853b3e33cc083e1043f130ad165bee5d6a7
Michail-Sergievsky <mikhail.sergiev@gmail.com>
2022-11-08 16:30:11 +0300

    Update soft

--- 2_grub_prepair.sh
+++ 2_grub_prepair.sh
@@ -6,7 +6,6 @@ pacman -Sy
 #time
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
-timedatectl set-ntp true
 #language
 sed -i '177s/.//' /etc/locale.gen
 sed -i '403s/.//' /etc/locale.gen
--- 5_soft.sh
+++ 5_soft.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed reflector
 reflector -c Russia -a 7 --sort rate --save /etc/pacman.d/mirrorlist
 
 #for system
-sudo pacman -S --needed alsa-utils arandr atool avahi bash-completion cifs-utils cmake cups dhcpcd fd feh flameshot fzf gvim htop i3-wm i3blocks i3lock i3status jq lib32-alsa-plugins lib32-libpulse libqalculate lsscsi lxqt-policykit man-pages neofetch noto-fonts-emoji ntfs-3g openvpn pacman-contrib papirus-icon-theme parted pavucontrol picom pkgfile pulseaudio pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack pulsemixer python-pip redshift rofi rsync rxvt-unicode rxvt-unicode-terminfo sbxkb speedtest-cli task telegram-desktop timew tldr tree ttf-droid ttf-liberation udiskie udisks2 ueberzug unzip urxvt-perls vifm w3m wget wireguard-tools xclip xdg-user-dirs xorg-apps xorg-docs xorg-server xorg-setxkbmap xorg-xinit xorg-xprop zip networkmanager-pptp networkmanager-openvpn networkmanager-openconnect networkmanager-l2tp network-manager-sstp rpm-tools nfs-utils tcpdump lsof cronie
+sudo pacman -S --needed alsa-utils arandr atool avahi bash-completion cifs-utils cmake cups dhcpcd fd feh flameshot fzf gvim htop i3-wm i3blocks i3lock i3status jq lib32-alsa-plugins lib32-libpulse libqalculate lsscsi lxqt-policykit man-pages neofetch noto-fonts-emoji ntfs-3g openvpn pacman-contrib papirus-icon-theme parted pavucontrol picom pkgfile pulseaudio pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack pulsemixer python-pip redshift rofi rsync rxvt-unicode rxvt-unicode-terminfo sbxkb speedtest-cli task telegram-desktop timew tldr tree ttf-droid ttf-liberation udiskie udisks2 ueberzug unzip urxvt-perls vifm w3m wget wireguard-tools xclip xdg-user-dirs xorg-apps xorg-docs xorg-server xorg-setxkbmap xorg-xinit xorg-xprop zip networkmanager-pptp networkmanager-openvpn networkmanager-openconnect networkmanager-l2tp network-manager-sstp rpm-tools nfs-utils tcpdump lsof cronie firefox
 
 #for CD-DVDROM
 sudo pacman -S --needed cdrtools dvd+rw-tools ccd2iso
@@ -28,7 +28,10 @@ yay -S --needed freac dropbox hunspell-ru hyphen-ru libreoffice-extension-langua
 
 # soft work
 sudo pacman -S --needed remmina vagrant virtualbox virtualbox-guest-iso virtualbox-host-modules-arch thunderbird docker
-yay -S --needed google-chrome rocketchat-client-bin 
+yay -S --needed google-chrome rocketchat-client-bin icaclient visual-studio-code-bin
+
+mkdir -p $HOME/.ICAClient/cache
+cp /opt/Citrix/ICAClient/config/{All_Regions,Trusted_Region,Unknown_Region,canonicalization,regions}.ini $HOME/.ICAClient/
 
 sudo downgrade wireshark-cli
 sudo downgrade wireshark-qt 
@@ -38,7 +41,7 @@ yay -S --needed powerpanel
 
 sudo pkgfile -u
 
-sudo systemctl --user enable greenclip.service
+systemctl --user enable greenclip.service
 sudo systemctl enable cups.service
 sudo systemctl enable sshd
 sudo systemctl enable avahi-daemon
@@ -46,6 +49,7 @@ sudo systemctl enable reflector.timer
 sudo systemctl enable reflector.service
 sudo systemctl enable pkgfile-update.timer
 sudo systemctl enable ly.service
+sudo timedatectl set-ntp true
 #for ssd
 sudo systemctl enable fstrim.service
 sudo systemctl enable fstrim.timer
