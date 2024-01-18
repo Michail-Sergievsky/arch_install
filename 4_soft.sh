eabb1d0544c0b00826649a4235f3fca1d9d957a9
Michail-Sergievsky <mikhail.sergiev@gmail.com>
2024-01-18 10:51:44 +0300

    Add plocate to soft & final config

--- 4_soft.sh
+++ 4_soft.sh
@@ -3,7 +3,7 @@ sudo pacman -S --needed reflector
 reflector -c Russia -a 7 --sort rate --save /etc/pacman.d/mirrorlist
 # echo $(grep "pacman soft" 5_soft.sh -A1 | tail -1)
 # pacman soft
-sudo pacman -S --needed alsa-utils arandr atool avahi bash-completion cifs-utils cmake cups dhcpcd fd feh flameshot fzf gvim htop i3-wm i3blocks i3lock i3status jq lib32-alsa-plugins lib32-libpulse libqalculate lsscsi lxqt-policykit man-pages neofetch ntfs-3g openvpn pacman-contrib papirus-icon-theme parted pavucontrol picom pkgfile pulseaudio pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack pulsemixer python-pip redshift rofi rsync rxvt-unicode rxvt-unicode-terminfo sbxkb speedtest-cli task telegram-desktop timew tldr tree ttf-droid ttf-liberation udiskie udisks2 ueberzug unzip urxvt-perls vifm w3m wget wireguard-tools xclip xdg-user-dirs xorg-apps xorg-docs xorg-server xorg-setxkbmap xorg-xinit xorg-xprop zip networkmanager-pptp networkmanager-openvpn networkmanager-openconnect networkmanager-l2tp network-manager-sstp rpm-tools nfs-utils tcpdump lsof cronie firefox languagetool pdfslicer libfaketime pdfgrep util-linux termdown bind thunderbird aegisub cmus discord exiv2 ffmpeg foliate hunspell-en_us hyphen-en jre8-openjdk lhasa libreoffice-fresh libreoffice-fresh-ru mediainfo mediainfo-gui mkvtoolnix-gui mp3info mupdf-tools opusfile p7zip pinta qbittorrent sxiv vlc zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps qrencode traceroute openresolv mtr handbrake obs-studio kdenlive inetutils android-file-transfer unrar bc strace net-tools shellcheck
+sudo pacman -S --needed alsa-utils arandr atool avahi bash-completion cifs-utils cmake cups dhcpcd fd feh flameshot fzf gvim htop i3-wm i3blocks i3lock i3status jq lib32-alsa-plugins lib32-libpulse libqalculate lsscsi lxqt-policykit man-pages neofetch ntfs-3g openvpn pacman-contrib papirus-icon-theme parted pavucontrol picom pkgfile pulseaudio pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack pulsemixer python-pip redshift rofi rsync rxvt-unicode rxvt-unicode-terminfo sbxkb speedtest-cli task telegram-desktop timew tldr tree ttf-droid ttf-liberation udiskie udisks2 ueberzug unzip urxvt-perls vifm w3m wget wireguard-tools xclip xdg-user-dirs xorg-apps xorg-docs xorg-server xorg-setxkbmap xorg-xinit xorg-xprop zip networkmanager-pptp networkmanager-openvpn networkmanager-openconnect networkmanager-l2tp network-manager-sstp rpm-tools nfs-utils tcpdump lsof cronie firefox languagetool pdfslicer libfaketime pdfgrep util-linux termdown bind thunderbird aegisub cmus discord exiv2 ffmpeg foliate hunspell-en_us hyphen-en jre8-openjdk lhasa libreoffice-fresh libreoffice-fresh-ru mediainfo mediainfo-gui mkvtoolnix-gui mp3info mupdf-tools opusfile p7zip pinta qbittorrent sxiv vlc zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps qrencode traceroute openresolv mtr handbrake obs-studio kdenlive inetutils android-file-transfer unrar bc strace net-tools shellcheck plocate
 
 #install yay
 git clone https://aur.archlinux.org/yay.git
@@ -13,7 +13,7 @@ cd ..
 rm -rf yay
 
 # yay soft
-yay -S --needed polybar dumptorrent hibernator ly nerd-fonts-source-code-pro rofi-blezz rofi-dmenu rofi-greenclip siji-git simple-mtpfs ttf-ms-fonts ttf-weather-icons update-grub urxvt-resize-font-git freac dropbox hunspell-ru hyphen-ru libreoffice-extension-languagetool transgui-gtk zoom anki goldendict-ng mcomix downgrade skypeforlinux-stable-bin pacman-cleanup-hook
+yay -S --needed polybar dumptorrent hibernator ly nerd-fonts-source-code-pro rofi-blezz rofi-dmenu rofi-greenclip siji-git simple-mtpfs ttf-ms-fonts ttf-weather-icons update-grub urxvt-resize-font-git freac dropbox hunspell-ru hyphen-ru libreoffice-extension-languagetool transgui-gtk zoom anki goldendict-ng mcomix downgrade skypeforlinux-stable-bin pacman-cleanup-hook icons-in-terminal-git
 
 #fonts
 sudo pacman -S --needed adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts otf-ipafont otf-ipaexfont otf-ipaexfont noto-fonts-emoji 
@@ -51,6 +51,7 @@ sudo systemctl enable --now reflector.timer
 sudo systemctl enable --now reflector.service
 sudo systemctl enable --now pkgfile-update.timer
 sudo systemctl enable --now ly.service
+sudo systemctl enable --now plocate-updatedb.timer 
 sudo timedatectl set-ntp true
 #for ssd
 sudo systemctl enable --now fstrim.service
--- 6_final.sh
+++ 6_final.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Exclude mount point from locate DB
+sudo echo 'PRUNEPATHS = "/run/media"' >> /etc/updatedb.conf
+
 #copy skel
 sudo cp -r /arch_install/files/skel/.bash_logout /etc/skel
 sudo cp -r /arch_install/files/skel/.bash_profile /etc/skel
