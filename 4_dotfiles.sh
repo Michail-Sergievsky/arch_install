f449b707f4d4bc4924f7aea2a94d699661f5790b
Michail-Sergievsky <mikhail.sergiev@gmail.com>
2025-04-05 23:50:03 +0300

    Update dotfiles

--- 4_dotfiles.sh
+++ 4_dotfiles.sh
@@ -6,6 +6,14 @@ if [ "$PWD" != "$HOME" ]; then
     exit 1
 fi
 
+# Check for internet connectivity
+if ping -c 4 8.8.8.8 &> /dev/null; then
+    echo "Continue"
+else
+    echo "CONNECT TO INTERNET!!!"
+    exit 1
+fi
+
 rm -rf $HOME/*
 rm -rf $HOME/.*
 git clone --bare https://github.com/Michail-Sergievsky/dotfiles.git $HOME/.myconf
@@ -26,5 +34,7 @@ mkdir -p $HOME/Media
 mkdir -p $HOME/Pictures/{Wall_use,screenshots,IMAGES_COPIED,IMAGES_SELECTED}
 mkdir -p $HOME/Phone
 mkdir -p $HOME/Temp
+mkdir -p $HOME/Videos
 mkdir -p $HOME/.soft
+mkdir -p $HOME/.cache/bash
 mkdir -p $HOME/.vim/undodir
